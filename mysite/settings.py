32408d2074f136c9d65dc4e6901b8843dc0faf3f
leeseungun1 <ma35146578@gmail.com>
2023-11-15 12:36:39 +0900

    allow host update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^!*$siuww%+z6nx=gy&yr^0*vbze%xb0e)!w14*@uz=i!du3c2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.compute-1.amazonaws.com']
 
 
 # Application definition
